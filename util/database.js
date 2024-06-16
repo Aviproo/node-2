62c640ab7c890147534c43f2f21ff6d5ff7fc608
AVINASH KUMAR <Aviproo>
2024-06-16 17:19:48 +0530

    Making SQL easy with Sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize("new_schema", "root", "123456", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "new_schema",
-  password: "123456",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
